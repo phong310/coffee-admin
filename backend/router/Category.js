5cc0c96958235372477daa7250f09d4d13052e49
phong310 <phamdinhphong12@gmail.com>
2023-05-04 17:18:28 +0700

    1

--- backend/router/Category.js
+++ backend/router/Category.js
@@ -1,24 +1,9 @@
 const router = require("express").Router();
 const CategoryController = require("../controller/Category");
 
-function checkPermission(req, res, next) {
-    // Kiểm tra xem người dùng đã đăng nhập hay chưa
-    if (!req.user) {
-        return res.status(401).send("Unauthorized");
-    }
-
-    // Kiểm tra xem người dùng có quyền được yêu cầu hay không
-    if (!req.user.permission || !req.user.permission.includes("permission_watch")) {
-        return res.status(403).send("Forbidden");
-    }
-
-    // Nếu người dùng có quyền, tiếp tục xử lý request
-    next();
-}
 
 
-
-router.get("/getAllCate", checkPermission, CategoryController.getAllCate);
+router.get("/getAllCate", CategoryController.getAllCate);
 
 router.post("/createCate", CategoryController.createCatelog);
 
